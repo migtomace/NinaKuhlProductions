8213483e1784ab98bb75183ea5f52034c53f047b
Miguel Acevedo <migtomace@gmail.com>
2018-12-19 17:10:11 -0800

    This commit updates the Kuhl Exchange page with a container.

--- includes/exchange.php
+++ includes/exchange.php
@@ -1,43 +1,44 @@
 <main id="exchange">
 
-    <div class="row text-center">
-        <h1>Kuhl Exchange</h1>
+    <div class="container">
+        <div class="row text-center">
+            <!--            start Kuhl Exchange-->
+            <div class="img-thumbnail">
+                <a href="https://www.facebook.com/LifeThruALensLLC/"><img src="images/KuhlExchange/lifeThroughALens.png" alt="Life Through A Lens" class="img-responsive"></a>
+            </div>
+            <div class="img-thumbnail">
+                <a href="https://www.bcmfilms.com/"><img src="images/KuhlExchange/bcmfilms.jpg" alt="BCM Films" class="img-responsive"></a>
+            </div>
+            <div class="img-thumbnail">
+                <a href="https://elkgrovewesternfestival.org/"><img src="images/KuhlExchange/westernFestival.png" alt="Elk Grove Western Festival" class="img-responsive"></a>
+            </div>
+            <div class="img-thumbnail">
+                <a href="https://www.carltonseniorliving.com/location/elk-grove/"><img src="images/events/carlton.jpg" alt="Songs for the ages" class="img-responsive"></a>
+            </div>
+            <div class="img-thumbnail">
+                <a href="http://tonyrsmusicpage.yolasite.com/"><img src="images/talents/tonyR.jpg" alt="Tony R Music" class="img-responsive"></a>
+            </div>
+            <div class="img-thumbnail">
+                <a href="http://nebula.wsimg.com/c7bf26e472244728751d1a5699da2fa5?AccessKeyId=C570626F18203D5146A0&disposition=0&alloworigin=1"><img src="images/KuhlExchange/skipsMusic.png" alt="Skips Music" class="img-responsive"></a>
+            </div>
+            <div class="img-thumbnail">
+                <a href="https://www.dgtheband.com/"><img src="images/KuhlExchange/dg.jpg" alt="Damaged Goods" class="img-responsive"></a>
+            </div>
+            <div class="img-thumbnail">
+                <a href="http://www.sinfultreats.net/"><img src="images/KuhlExchange/sinfultreats.jpg" alt="Sinful Treats" class="img-responsive"></a>
+            </div>
+            <div class="img-thumbnail">
+                <a href="https://www.facebook.com/groups/sinsationalconfections/"><img src="images/KuhlExchange/sinsationalconfessions.jpg" alt="Sinsational Confessions" class="img-responsive"></a>
+            </div>
+            <div class="img-thumbnail">
+                <a href="http://www.artisanfinishes.com/wordpress/"><img src="images/KuhlExchange/artisan.jpg" alt="Artisan Finishes" class="img-responsive"></a>
+            </div>
+            <div class="img-thumbnail">
+                <a href="https://www.facebook.com/soundsofninakuhl/"><img src="images/KuhlExchange/sounds.jpg" alt="The sounds of Nina Kuhl Productions"></a>
+            </div>
     </div>
-    <div class="row text-center">
-        <!--            start Kuhl Exchange-->
-        <div class="img-thumbnail">
-            <a href="https://www.facebook.com/LifeThruALensLLC/"><img src="images/KuhlExchange/lifeThroughALens.png" alt="Life Through A Lens" class="img-responsive"></a>
-        </div>
-        <div class="img-thumbnail">
-            <a href="https://www.bcmfilms.com/"><img src="images/KuhlExchange/bcmfilms.jpg" alt="BCM Films" class="img-responsive"></a>
-        </div>
-        <div class="img-thumbnail">
-            <a href="https://elkgrovewesternfestival.org/"><img src="images/KuhlExchange/westernFestival.png" alt="Elk Grove Western Festival" class="img-responsive"></a>
-        </div>
-        <div class="img-thumbnail">
-            <a href="https://www.carltonseniorliving.com/location/elk-grove/"><img src="images/events/carlton.jpg" alt="Songs for the ages" class="img-responsive"></a>
-        </div>
-        <div class="img-thumbnail">
-            <a href="http://tonyrsmusicpage.yolasite.com/"><img src="images/talents/tonyR.jpg" alt="Tony R Music" class="img-responsive"></a>
-        </div>
-        <div class="img-thumbnail">
-            <a href="http://nebula.wsimg.com/c7bf26e472244728751d1a5699da2fa5?AccessKeyId=C570626F18203D5146A0&disposition=0&alloworigin=1"><img src="images/KuhlExchange/skipsMusic.png" alt="Skips Music" class="img-responsive"></a>
-        </div>
-        <div class="img-thumbnail">
-            <a href="https://www.dgtheband.com/"><img src="images/KuhlExchange/dg.jpg" alt="Damaged Goods" class="img-responsive"></a>
-        </div>
-        <div class="img-thumbnail">
-            <a href="http://www.sinfultreats.net/"><img src="images/KuhlExchange/sinfultreats.jpg" alt="Sinful Treats" class="img-responsive"></a>
-        </div>
-        <div class="img-thumbnail">
-            <a href="https://www.facebook.com/groups/sinsationalconfections/"><img src="images/KuhlExchange/sinsationalconfessions.jpg" alt="Sinsational Confessions" class="img-responsive"></a>
-        </div>
-        <div class="img-thumbnail">
-            <a href="http://www.artisanfinishes.com/wordpress/"><img src="images/KuhlExchange/artisan.jpg" alt="Artisan Finishes" class="img-responsive"></a>
-        </div>
-        <div class="img-thumbnail">
-            <a href="https://www.facebook.com/soundsofninakuhl/"><img src="images/KuhlExchange/sounds.jpg" alt="The sounds of Nina Kuhl Productions"></a>
-        </div>
+
+
 
         <!--            end Kuhl Exchange-->
 
