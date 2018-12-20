db2abec8600e236ab1d12a8ad4b9d76ca68f3853
Miguel Acevedo <migtomace@gmail.com>
2018-12-19 23:08:30 -0800

    This commit updates the home carousel with CROPPED construction photo's.

--- includes/home.php
+++ includes/home.php
@@ -9,55 +9,55 @@
         <!-- Wrapper for slides -->
         <div class="carousel-inner">
             <div class="item active">
-                <img src="images/home/construction1.jpg" alt="Construction">
+                <img src="images/home/c1.jpg" alt="Construction">
             </div>
 
             <div class="item">
-                <img src="images/home/construction2.jpg" alt="Construction">
+                <img src="images/home/c2.jpg" alt="Construction">
             </div>
 
             <div class="item">
-                <img src="images/home/construction3.jpg" alt="Construction">
+                <img src="images/home/c3.jpg" alt="Construction">
             </div>
 
             <div class="item">
-                <img src="images/home/construction4.jpg" alt="Construction">
+                <img src="images/home/c4.jpg" alt="Construction">
             </div>
 
             <div class="item">
-                <img src="images/home/construction5.jpg" alt="Construction">
+                <img src="images/home/c5.jpg" alt="Construction">
             </div>
 
             <div class="item">
-                <img src="images/home/construction6.jpg" alt="Construction">
+                <img src="images/home/c6.jpg" alt="Construction">
             </div>
 
             <div class="item">
-                <img src="images/home/construction7.jpg" alt="Construction">
+                <img src="images/home/c7.jpg" alt="Construction">
             </div>
 
             <div class="item">
-                <img src="images/home/construction8.jpg" alt="Construction">
+                <img src="images/home/c8.jpg" alt="Construction">
             </div>
 
             <div class="item">
-                <img src="images/home/construction9.jpg" alt="Construction">
+                <img src="images/home/c9.jpg" alt="Construction">
             </div>
 
             <div class="item">
-                <img src="images/home/construction10.jpg" alt="Construction">
+                <img src="images/home/c10.jpg" alt="Construction">
             </div>
 
             <div class="item">
-                <img src="images/home/construction11.jpg" alt="Construction">
+                <img src="images/home/c11.jpg" alt="Construction">
             </div>
 
             <div class="item">
-                <img src="images/home/construction12.jpg" alt="Construction">
+                <img src="images/home/c12.jpg" alt="Construction">
             </div>
 
             <div class="item">
-                <img src="images/home/construction13.jpg" alt="Construction">
+                <img src="images/home/c13.jpg" alt="Construction">
             </div>
         </div>
 
