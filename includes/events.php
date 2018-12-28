c1defe66c6f15b8967862725745e96697d4be47f
Miguel Acevedo <migtomace@gmail.com>
2018-12-27 20:14:19 -0800

    This commit fixes the thumbnail size in the Events page

--- includes/events.php
+++ includes/events.php
@@ -2,12 +2,12 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-md-6">
+            <div class="img-thumbnail">
                 <a href="https://www.lowtonofilmfest.com/">
                     <img src="images/events/low2no.png" alt="Low to No" class="img-responsive">
                 </a>
             </div>
-            <div class="col-md-6">
+            <div class="img-thumbnail">
                 <img src="images/events/carlton.jpg" alt="Songs for the ages" class="img-responsive">
             </div>
         </div>
